65bc19137e7defde220ed325777f7815d46332a2
Veronica N A Jack <43711657+VNAJack@users.noreply.github.com>
2020-02-18 00:13:28 -0800

    Update foodPostValidation.js
    changed the mainForm getElementbyId to reflect the main id in the html page.
    Added a couple of checks for new fields (image and source).

--- jsExternal/foodPostValidation.js
+++ jsExternal/foodPostValidation.js
@@ -1,30 +1,39 @@
 function checkBlank(input){
   if (input == '') {
     return true;
-} else return false;
+  } else{
+    return false;
+}
 }
 
 window.onload = function(){
-  var mainForm = document.getElementById("postArea");
+  var mainForm = document.getElementById("leftoversForm");
 
 
   mainForm.onsubmit = function(e){
+  var inputedImage = document.forms["makePost"]["foodImage"].value;
   var inputedTime = document.forms["makePost"]["safeUntil"].value;
   var inputedFood = document.forms["makePost"]["typeOfFood"].value;
   var inputedLocation = document.forms["makePost"]["foodLocation"].value;
+  var inputedSource = document.forms["makePost"]["source"].value;
 
 
-  if (checkBlank(inputedTime)) {
-    alert("Please select how much longer the food will last until it can no longer be consumed.");
+  if (checkBlank(inputedImage)){
+    alert("Please provide an image of the leftovers.");
+    e.preventDefault();
+  }else if (checkBlank(inputedTime)) {
+    alert("Please select how much longer the leftovers will be fresh. This is the length of time until it can no longer be consumed safely (unless otherwise indicated by the food donor.");
     e.preventDefault();
   } else if (checkBlank(inputedFood)){
-    alert("Tell us what items you picked up.");
+    alert("Please list the food items you picked up.");
     e.preventDefault();
   }else if (checkBlank(inputedLocation)){
-    alert("Please type the location of where you're leaving the food.");
+    alert("Please indicate where you are taking the leftovers.");
     e.preventDefault();
-  }
-  else {
+  }else if(checkBlank(inputedSource)){
+    alert("Please provide the source of the leftovers.");
+    e.preventDefault();
+  }else {
     return true;
   }
 }
