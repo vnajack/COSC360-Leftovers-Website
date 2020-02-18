c8f9720007118586fea792399a8a4aac016e4578
Veronica N A Jack <43711657+VNAJack@users.noreply.github.com>
2020-02-18 00:26:24 -0800

    Update foodDonorValidation.js
    in the select.onchange, I added document.getElementById("createTextbox").innerHTML=""; so that if you switch options, then the content in the textbox disappears.

--- jsExternal/foodDonorValidation.js
+++ jsExternal/foodDonorValidation.js
@@ -1,13 +1,14 @@
 
 function checkDonor(){
-	var inputedDonor = document.forms["donorSubmission"]["donor"].value; //value for whichever select option the user chose
+  var inputedDonor = document.forms["donorSubmission"]["donor"].value; //value for whichever select option the user chose
   var createTextbox = document.getElementById("createTextbox"); //the div in foodDonorForm.html
+
 	if (inputedDonor == "onCampusEvent"){ //if the user chose "On-campus Event"
-		// In class we used document.write(), but that was creating an entirely new page outside of the form (?). The innerHTML method inserts it into the div instead
-		createTextbox.innerHTML = createTextbox.innerHTML + "<label> Tell us the name of the event </label><br><input type='text' name='eventName' size=50><br><label>Where can the food be picked up from?</label><br><input type='text' name='eventLocation' size=50>"
+		//The innerHTML method inserts it into the div instead of a separate file
+		createTextbox.innerHTML = createTextbox.innerHTML + "<p><label>What is the name of the event?</label><input type='text' name='eventName'></p><p><label>Where can we pick up the leftovers?</label><input type='text' name='eventLocation'></p>"
 		//inputedDonor.innerHTML = "<label> Tell us the name of the event as well as where the food can be picked up from</label><br><input type='text' name='nameEvent'>";
 	} if (inputedDonor == "other"){
-		createTextbox.innerHTML = createTextbox.innerHTML + "<p><label>If you aren't already a trusted donor please enter how we can contact you:</label><input type='text' name='donorContact'></p><p> You can also email our executives to become a trusted donor! <a href='mailto:leftoversubco@gmail.com?Subject=Food%20Provider'><img src='images/email_16.png'></a> "
+		createTextbox.innerHTML = createTextbox.innerHTML + "<p> Email our executives to become a trusted donor! <a href='mailto:leftoversubco@gmail.com?Subject=New%20Food%20Provider'><img src='images/email_16.png'></a> "
 	}
 }
 
@@ -24,16 +25,18 @@ window.onload = function(){
 
 
 	select.onchange = function(){ //occurs whenever a new select option is clicked
+		document.getElementById("createTextbox").innerHTML="";
 		checkDonor(); //goes to checkDonor function
 	}
 
   mainForm.onsubmit = function(e){ //for when the form is submitted
 
 	var inputedItems = document.forms["donorSubmission"]["foodDescription"].value;
+	var inputedPicture = document.forms["donorSubmission"]["picture"].value;
 	var inputedKiloGrams = document.forms["donorSubmission"]["kg"].value;
 	var inputedDate = document.forms["donorSubmission"]["pickUpDate"].value;
 
-	 if (checkBlank(inputedItems) || checkBlank(inputedKiloGrams) || checkBlank(inputedDate)){
+	 if (checkBlank(inputedItems) || checkBlank(inputedPicture) || checkBlank(inputedKiloGrams) || checkBlank(inputedDate)){
 		alert("Please fill out all of the required fields");
 		e.preventDefault();
 	}
